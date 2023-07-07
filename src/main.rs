1d6bde345c0c5b0256c9f500fa450dd42bedf912
maksasj <radioboos@gmail.com>
2023-07-07 22:54:50 +0300

    some things

--- src/main.rs
+++ src/main.rs
@@ -40,27 +40,144 @@ fn main() {
         }))
         .insert_resource(Msaa::Off)
         .add_startup_system(setup)
-        .add_system(growth)
+        .add_systems((growth_system, seed_system, branch_system).chain())
         .run();
 }
 
 #[derive(Component)]
+pub struct Growth {
+    pub timer: f32,
+    pub max_time: f32,
+    pub done: bool
+}
+
+impl Growth {
+    pub fn new(max_growth_time: f32) -> Self {
+        Growth { 
+            timer: 0.0,
+            max_time: max_growth_time,
+            done: false
+        }
+    }
+}
+
+#[derive(Component)]
 pub struct Seed {
-    pub timer: f32
 }
 
 impl Seed {
     pub fn new() -> Self {
-        Seed { 
-            timer: 0.0 
+        Seed { }
+    }
+}
+
+#[derive(Component)]
+pub struct Branch {
+}
+
+impl Branch {
+    pub fn new() -> Self {
+        Branch { }
+    }
+}
+
+#[derive(Bundle)]
+pub struct BranchBundle {
+    sprite: SpriteBundle,
+    
+    growth: Growth,
+    branch: Branch,
+}
+
+
+impl BranchBundle {
+    pub fn new_from_transform(mut meshes: &mut ResMut<Assets<Mesh>>, mut materials: &mut ResMut<Assets<ColorMaterial>>, transform: Transform) -> Self {
+        BranchBundle { 
+            sprite: SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(0.25, 0.25, 0.75),
+                    custom_size: Some(Vec2::new(25.0, 25.0)),
+                    ..default()
+                },
+                transform: transform,
+                ..default()
+            },
+
+            growth: Growth::new(15.0),
+            branch: Branch::new()
         }
     }
+}
+
+pub fn branch_system(
+    mut commands: Commands,  
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut targets: Query<(&mut Growth, &mut Transform), With<Branch>>
+) {
+
+for (mut growth, mut transform) in targets.iter_mut() {
+    // Still growing
+    if growth.timer < growth.max_time {
+        transform.scale.y = 0.05 * growth.timer;
+        continue;
+    }
+
+    // Nice we growth up !
+    if growth.done {
+        continue; // We have growth up, last iteration i guess
+    }
+    
+    growth.done = true;
+
+    let first_pass_layer = RenderLayers::layer(1);
+
+    // Circle
+    let mut new_transform: Transform = transform.clone();
+    new_transform.translation.y += 25.0 * transform.scale.y;
+
+    commands.spawn((SeedBundle::new_from_transform(&mut meshes, &mut materials, new_transform), first_pass_layer));
+}   
+}
+
+pub fn seed_system(
+        mut commands: Commands,  
+        mut meshes: ResMut<Assets<Mesh>>,
+        mut materials: ResMut<Assets<ColorMaterial>>,
+        mut targets: Query<(&mut Growth, &mut Transform), With<Seed>>
+    ) {
+
+    for (mut growth, mut transform) in targets.iter_mut() {
+        // Still growing
+        if growth.timer < growth.max_time {
+            transform.scale = 0.2 * Vec3::splat(growth.timer);
+            continue;
+        }
+
+
+        // Nice we growth up !
+        if growth.done {
+            continue; // We have growth up, last iteration i guess
+        }
+        
+        growth.done = true;
+
+        let first_pass_layer = RenderLayers::layer(1);
+
+        // Circle
+        let mut new_transform: Transform = transform.clone();
+        new_transform.translation.y += 2.0;
+
+        commands.spawn((BranchBundle::new_from_transform(&mut meshes, &mut materials, new_transform), first_pass_layer));
+    }   
 }
 
 #[derive(Bundle)]
 pub struct SeedBundle {
     mesh: MaterialMesh2dBundle<ColorMaterial>,
-    seed: Seed
+    
+    growth: Growth,
+    seed: Seed,
 }
 
 impl SeedBundle {
@@ -72,29 +189,31 @@ impl SeedBundle {
                 transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
                 ..default()
             },
+            growth: Growth::new(2.0),
+            seed: Seed::new()
+        }
+    }
+
+    pub fn new_from_transform(mut meshes: &mut ResMut<Assets<Mesh>>, mut materials: &mut ResMut<Assets<ColorMaterial>>, transform: Transform) -> Self {
+        SeedBundle { 
+            mesh: MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::new(10.).into()).into(),
+                material: materials.add(ColorMaterial::from(Color::PURPLE)),
+                transform: transform,
+                ..default()
+            },
+            growth: Growth::new(2.0),
             seed: Seed::new()
         }
     }
 }
 
-#[derive(Component)]
-pub struct Branch {
-    pub timer: f32
-}
-
-impl Branch {
-    pub fn new() -> Self {
-        Branch { 
-            timer: 0.0 
+pub fn growth_system(mut commands: Commands, mut targets: Query<(&mut Growth)>) {
+    for mut growth in targets.iter_mut() {
+        growth.timer += 0.01;
+        if growth.timer > growth.max_time {
+            growth.timer = growth.max_time;
         }
-    }
-}
-
-pub fn growth(mut commands: Commands, mut targets: Query<(&mut Seed, &mut Transform)>) {
-    for (mut seed, mut transform) in targets.iter_mut() {
-        seed.timer += 0.001;
-
-        transform.scale = Vec3::splat(seed.timer);
     }
 }
 
